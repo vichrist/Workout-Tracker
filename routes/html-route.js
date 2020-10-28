d66ba5181bb12d1c96dcfb4b6870fe05c82613e4
Christal Vigil <c.vigil27@gmail.com>
2020-10-27 23:12:28 -0600

    updating html routes

---
+++ routes/html-route.js
@@ -0,0 +1,20 @@
+// requiring in models folder and path dependency  
+const db = require('../models')
+const path = require('path');
+
+// exporting the function and having it execute upon user request for CRUD methods 
+module.exports = (app) => {
+    
+//static page that will render the index file 
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+//static page that will render the exercise file 
+    app.get('/exercise', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    });
+//static page that will render the stats file 
+    app.get('/stats', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/stats.html'));
+    });
+}; 
